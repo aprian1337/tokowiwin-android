2f2acdaaa031e81279edcd6532bc86b38322b5cc
aprian1337 <dwikyapriyan@gmail.com>
2023-01-04 19:05:40 +0700

    update

--- app/src/main/java/com/tokowiwin/tokowiwin/utils/Constants.kt
+++ app/src/main/java/com/tokowiwin/tokowiwin/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.tokowiwin.tokowiwin.utils
 
 
 object Constants {
-    const val BASE_URL = "http://172.18.0.1:9091/"
+    const val BASE_URL = "http://ec2-3-88-221-13.compute-1.amazonaws.com:9091/"
     const val MERCHANT_ID = "G678927804"
     const val CLIENT_KEY = "SB-Mid-client-C3L_iMoI-u_LRGUm\n"
     const val SERVER_KEY = "SB-Mid-server-3vGLH7nAIlbZYNt7ftfTLcry\n"
